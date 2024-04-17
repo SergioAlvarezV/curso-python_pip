52c892d7c2be24d7252b09eff817916b1d67b9ff
SergioAlvarezV <sergioandresalvarezvargas5@gmail.com>
2024-04-17 12:14:47 -0500

    add file and subfile for request an http with requests

---
+++ web-server/store.py
@@ -0,0 +1,12 @@
+import requests
+
+'https://api.escuelajs.co/api/v1/categories'
+
+def get_catergories():
+    r = requests.get('https://api.escuelajs.co/api/v1/categories')
+    print(r.status_code)
+    print(r.text)
+    print(type(r.text))
+    categories = r.json()
+    for category in categories:
+        print(category['name'])
