c7237eeef694078ff53aea69eba6dbf014656d42
SergioAlvarezV <sergioandresalvarezvargas5@gmail.com>
2024-04-22 15:06:36 -0500

    add docs in add files in web-server for uvicorn fastAPI and Docker

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,28 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return[1,2,3]
+
+@app.get('/contac', response_class=HTMLResponse)
+def get_list():
+    return """
+        <title>Web-server</title>
+        <h1>Hola soy una pagina</h1>
+        <p>soy un parrafo</p>
+     """ 
+
+@app.get('/Soffi-web', response_class=HTMLResponse)
+def get_list():
+    return """
+        <title>Sofi-Web</title>
+        <h1>Hola Sofi Bienvenida a Soffi-Web</h1>
+        <p>Soffi-Web es una pagina creada para ti</p>
+     """
 
 def run():
     store.get_catergories()
